045f118ac32973bf12b37f25154f9b6b4624b186
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-03-03 23:31:14 +0100

    add Spring initialized project

---
+++ workdone-server/src/main/java/pl/pr0gramista/WorkdoneApplication.java
@@ -0,0 +1,12 @@
+package pl.pr0gramista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkdoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkdoneApplication.class, args);
+	}
+}
