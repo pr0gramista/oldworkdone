965008d726d757afbed7fff4d4413cbc245b9d14
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-03-24 02:10:21 +0100

    Refactor or delete Vue.js functions

---
+++ src/main/resources/public/app/app.js
@@ -0,0 +1,69 @@
+var habitRepository = {
+  fetch: function () {
+    axios.get("/habit/").then(function (r) {
+      app.habits = r.data;
+    })
+  },
+  save: function (habit) {
+    axios.put("/habit/" + habit.id, habit);
+  }
+}
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    habits: habitRepository.fetch()
+  },
+  watch: {
+  },
+  methods: {
+    addNewHabit: function () {
+      
+    },
+    refreshTodos: function () {
+
+    }
+  }
+})
+
+Vue.component('habit', {
+  props: ['habit'],
+  data: function () {
+    return {
+      is_editing: false,
+      edit_text: ''
+    }
+  },
+  watch: {
+    habit: {
+      handler: function (habit) {
+        habitRepository.save(habit);
+      },
+      deep: true
+    }
+  },
+  methods: {
+    startEdit: function () {
+      this.is_editing = true;
+      this.edit_text = this.habit.text;
+    },
+    endEdit: function () {
+      this.is_editing = false;
+      this.habit.text = this.edit_text;
+    }
+  },
+  template:
+  `<div class="habit col s12 m6 l4">
+    <div class="card">
+      <div class="card-content">
+        <i v-if="!is_editing" @click="startEdit" class="material-icons right">edit</i>
+        <i v-if="is_editing" @click="endEdit" class="material-icons right">done</i>
+        <p v-if="!is_editing">{{ habit.text }}</p>
+        <textarea v-if="is_editing" v-model="edit_text" />
+      </div>
+      <div class="card-action">
+        <button class="waves-effect waves-light btn">Done</button>
+      </div>
+    </div>
+  </div>`
+})
