de0ac1dce232531044a7f94d5d5345b85afae2dc
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-04-26 04:05:06 +0200

    Add tests for completing todo and tasks

--- src/test/java/pl/pr0gramista/todo/TodoControllerTest.java
+++ src/test/java/pl/pr0gramista/todo/TodoControllerTest.java
@@ -16,6 +16,7 @@ import pl.pr0gramista.user.User;
 import pl.pr0gramista.user.UserService;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -52,6 +53,8 @@ public class TodoControllerTest {
     private String exampleTodoJson;
     private Todo exampleTodo2;
     private String exampleTodoJson2;
+    private Todo exampleTodo3;
+    private String exampleTodoJson3;
 
     @Before
     public void before() throws JsonProcessingException {
@@ -71,14 +74,30 @@ public class TodoControllerTest {
         exampleTodoJson = mapper.writeValueAsString(exampleTodo);
         System.out.println(exampleTodoJson);
 
+        Task taskDone = new Task("Make a snowman");
+        taskDone.setDone(true);
+
         //Make second example
         exampleTodo2 = new Todo.TodoBuilder("Winter time")
                 .withTask(new Task("Take a long nap"))
                 .withTask(new Task("Throw snowballs"))
-                .withTask(new Task("Make a snowman"))
+                .withTask(taskDone)
                 .build();
         exampleTodoJson2 = mapper.writeValueAsString(exampleTodo2);
         System.out.println(exampleTodoJson2);
+
+        //Make second example
+        exampleTodo3 = new Todo.TodoBuilder("Winter time")
+                .withTask(new Task("Take a long nap"))
+                .withTask(new Task("Throw snowballs"))
+                .withTask(new Task("Make a sandwich"))
+                .build();
+        exampleTodo3.getTasks().forEach(task -> {
+            task.setDone(true);
+            task.setRewarded(true);
+        });
+        exampleTodoJson3 = mapper.writeValueAsString(exampleTodo3);
+        System.out.println(exampleTodoJson3);
     }
 
     @Test
@@ -127,6 +146,69 @@ public class TodoControllerTest {
         verify(todoRepository, atMost(1)).save(exampleTodo2WithId);
 
         verify(todoValidator, atLeastOnce()).validate(any(Todo.class), any());
+    }
+
+    @Test
+    public void updateCompletesTasks() throws Exception {
+        when(todoRepository.findOneByIdAndOwner(eq(1L), any(User.class))).thenReturn(Optional.of(exampleTodo));
+
+        Task taskDone = exampleTodo2.getTasks().get(2);
+        TaskCompletion taskCompletion = new TaskCompletion(500, 300, null, taskDone);
+        when(taskService.completeTask(any(Task.class), any(Todo.class))).thenReturn(Optional.of(taskCompletion));
+
+        mockMvc.perform(put("/todo/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(exampleTodoJson2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(mvcResult -> {
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    List<TaskCompletion> receivedCompletions = mapper.readValue(
+                            mvcResult.getResponse().getContentAsString(),
+                            mapper.getTypeFactory().constructCollectionType(List.class, TaskCompletion.class));
+
+                    assertThat(receivedCompletions.size(), greaterThan(0));
+                    TaskCompletion receivedCompletion = receivedCompletions.get(0);
+
+                    assertThat(receivedCompletion.getTask(), is(taskDone));
+                    assertThat(receivedCompletion.getExperience(), is(500));
+                    assertThat(receivedCompletion.getCoins(), is(300));
+                    assertThat(receivedCompletion.getDateTime(), is(nullValue()));
+                });
+
+        verify(taskService, atMost(1)).completeTask(eq(taskDone), eq(exampleTodo2));
+    }
+
+    @Test
+    public void updateCompletesTodo() throws Exception {
+        when(todoRepository.findOneByIdAndOwner(eq(1L), any(User.class))).thenReturn(Optional.of(exampleTodo));
+
+        TodoCompletion todoCompletion = new TodoCompletion(500, 300, null, exampleTodo3);
+        when(todoService.complete(any(Todo.class))).thenReturn(Optional.of(todoCompletion));
+
+        mockMvc.perform(put("/todo/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(exampleTodoJson3))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(mvcResult -> {
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    List<TodoCompletion> receivedCompletions = mapper.readValue(
+                            mvcResult.getResponse().getContentAsString(),
+                            mapper.getTypeFactory().constructCollectionType(List.class, TodoCompletion.class));
+
+                    assertThat(receivedCompletions.size(), greaterThan(0));
+                    TodoCompletion receivedCompletion = receivedCompletions.get(0);
+
+                    assertThat(receivedCompletion.getTodo(), is(exampleTodo3));
+                    assertThat(receivedCompletion.getExperience(), is(500));
+                    assertThat(receivedCompletion.getCoins(), is(300));
+                    assertThat(receivedCompletion.getDateTime(), is(nullValue()));
+                });
+
+        verify(todoService, atMost(1)).complete(eq(exampleTodo3));
     }
 
     @Test
